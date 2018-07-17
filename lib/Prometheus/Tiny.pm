6e34858698b2911e1c4a4378ccf8c8ab747d5b5a
Rob N ★ <robn@robn.io>
2018-07-18 07:25:35 +1000

    Metric pre-declaration with help & type

--- lib/Prometheus/Tiny.pm
+++ lib/Prometheus/Tiny.pm
@@ -9,6 +9,7 @@ sub new {
   my ($class) = @_;
   return bless {
     metrics => {},
+    meta => {},
   }, $class;
 }
 
@@ -23,16 +24,29 @@ sub set {
   return;
 }
 
+sub declare {
+  my ($self, $name, %meta) = @_;
+  $self->{meta}{$name} = { %meta };
+  return;
+}
+
 sub format {
   my ($self) = @_;
+  my %names = map { $_ => 1 } (keys %{$self->{metrics}}, keys %{$self->{meta}});
   return join '', map {
     my $name = $_;
-    map {
-      $_ ?
-        join '', $name, '{', $_, '} ', $self->{metrics}{$name}{$_}, "\n" :
-        join '', $name, ' ', $self->{metrics}{$name}{$_}, "\n"
-    } sort keys %{$self->{metrics}{$name}};
-  } sort keys %{$self->{metrics}};
+    (
+      (defined $self->{meta}{$name}{help} ?
+        ("# HELP $name $self->{meta}{$name}{help}\n") : ()),
+      (defined $self->{meta}{$name}{type} ?
+        ("# TYPE $name $self->{meta}{$name}{type}\n") : ()),
+      (map {
+        $_ ?
+          join '', $name, '{', $_, '} ', $self->{metrics}{$name}{$_}, "\n" :
+          join '', $name, ' ', $self->{metrics}{$name}{$_}, "\n"
+      } sort keys %{$self->{metrics}{$name}}),
+    )
+  } sort keys %names;
 }
 
 1;
